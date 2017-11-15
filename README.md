19837a869b94e6e591454e28c86a1fe5b207bef4
Lennon Hill <@>
2017-11-16 00:40:23 +0100

    Initial project import

---
+++ README.md
@@ -0,0 +1,8 @@
+## cookies.txt
+
+Firefox Add-on which exports all cookies to a Netscape HTTP Cookie File, as used by curl, wget, and youtube-dl, among others.
+
+## Licence
+
+<a href="https://github.com/lennonhill/cookies-txt/blob/master/LICENSE">GPLv3</a>
+
---
+++ background.js
@@ -0,0 +1,37 @@
+
+function formatCookie(co) {
+  return [
+    [
+      co.httpOnly ? '#HttpOnly_' : '',
+      !co.hostOnly && co.domain && !co.domain.startsWith('.') ? '.' : '',
+      co.domain
+    ].join(''),
+    co.hostOnly ? 'FALSE' : 'TRUE',
+    co.path,
+    co.secure ? 'TRUE' : 'FALSE',
+    co.session || !co.expirationDate ? 0 : co.expirationDate,
+    co.name,
+    co.value + '\n'
+  ].join('\t');
+}
+
+function saveCookies(cookies) {
+  var header = [
+    '# Netscape HTTP Cookie File\n',
+    '# https://curl.haxx.se/rfc/cookie_spec.html\n',
+    '# This is a generated file! Do not edit.\n\n'
+  ];
+  var body = cookies.map(formatCookie)
+  var blob = new Blob(header.concat(body), {type: 'text/plain'});
+  var objectURL = URL.createObjectURL(blob);
+  browser.downloads.download({url: objectURL, filename: 'cookies.txt',
+    saveAs: true, conflictAction: 'overwrite'});
+}
+
+function handleClick() {
+  var gettingAll = browser.cookies.getAll({});
+  gettingAll.then(saveCookies);
+}
+
+browser.browserAction.onClicked.addListener(handleClick);
+
