fb584e392260d12416733785c8dc13a453361c2d
Michele C. Soccio <michele@soccio.it>
2017-12-11 11:56:47 +0100

    Multi Container Support. Save one file for each container

--- background.js
+++ background.js
@@ -1,4 +1,3 @@
-
 function formatCookie(co) {
   return [
     [
@@ -15,22 +14,55 @@ function formatCookie(co) {
   ].join('\t');
 }
 
-function saveCookies(cookies) {
+async function getCookiesFilename(storeId) {
+  if (storeId == 'firefox-default') { 
+    return 'cookies.txt' 
+  } else {
+    let container;
+    try {
+      container = await browser.contextualIdentities.get(storeId); 
+    } catch (e) {
+      /* In case we can't get the name of the container, fallback on the storeId */
+      container = storeId
+    }
+    return 'cookies.' + container.name + '.txt'
+  }
+}
+
+async function saveCookies(cookies) {
   var header = [
     '# Netscape HTTP Cookie File\n',
     '# https://curl.haxx.se/rfc/cookie_spec.html\n',
     '# This is a generated file! Do not edit.\n\n'
   ];
   var body = cookies.map(formatCookie)
+  let storeId = cookies[0].storeId
   var blob = new Blob(header.concat(body), {type: 'text/plain'});
   var objectURL = URL.createObjectURL(blob);
-  browser.downloads.download({url: objectURL, filename: 'cookies.txt',
-    saveAs: true, conflictAction: 'overwrite'});
+  let cookiesFilename = await getCookiesFilename(storeId)
+  browser.downloads.download(
+    {
+      url: objectURL, 
+      filename: cookiesFilename,
+      saveAs: true, 
+      conflictAction: 'overwrite'
+    }
+  );
+}
+
+function getCookies(stores) {
+  for (var store of stores) {
+    console.log("Store: " + store.id)
+    var gettingAll = browser.cookies.getAll({
+        storeId: store.id
+    });
+    gettingAll.then(saveCookies);
+  }
 }
 
 function handleClick() {
-  var gettingAll = browser.cookies.getAll({});
-  gettingAll.then(saveCookies);
+  var gettingAllStores = browser.cookies.getAllCookieStores()
+  gettingAllStores.then(getCookies)
 }
 
 browser.browserAction.onClicked.addListener(handleClick);
